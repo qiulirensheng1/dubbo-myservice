0652ace9e2102b13986fc61481431fc774987b8a
邱立 <867412743@qq.com>
2018-10-14 22:29:28 +0800

    Revert "新建分支111"
    This reverts commit fc1a0c5

--- src/main/java/com/qiuli/cn/ResetTest.java
+++ src/main/java/com/qiuli/cn/ResetTest.java
@@ -2,14 +2,12 @@ package com.qiuli.cn;
 
 /**
  * @author: qiuli
- * @date: 2018/10/14 21:33
+ * @date: 2018/10/14 22:29
  * @description:
  */
 
 
-public class DubboTest {
-
-
+public class ResetTest {
     public static void main(String[] args) {
 
     }
